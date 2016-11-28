8419c00576ac37baf15d1b798961b80a4beb0b31
Boris Popov <popov@whitekefir.ru>
2016-11-28 16:50:00 +0300

    add some modules

---
+++ src/forker.rs
@@ -0,0 +1,11 @@
+//
+// Copyright (c) 2016, Boris Popov <popov@whitekefir.ru>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+pub fn hello() {
+
+}
