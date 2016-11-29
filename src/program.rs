7a425d25fecbff55e5e79060181aa3bf831e4bac
Boris Popov <popov@whitekefir.ru>
2016-11-29 15:43:24 +0300

    begin mod program

---
+++ src/program.rs
@@ -0,0 +1,37 @@
+//
+// Copyright (c) 2016, Boris Popov <popov@whitekefir.ru>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+//////////////////////////////////////////////////////////////////
+pub static NAME:    &'static str = "MountAndExec";
+pub static VERSION: &'static str = "0.1.0";
+//////////////////////////////////////////////////////////////////
+/*
+pub struct Info {
+
+    pub name:    &'static str,
+    version: &'static str
+}
+
+impl Info {
+    pub fn init(&mut self) {
+        self.name = &NAME;
+        //self.version = VERSION.to_string();
+        
+        //self.name = "MountAndExec";
+        //
+        //TODO
+        //
+        //return;
+    }
+*/
+    /*
+    fn area(&self) -> f64 {
+        std::f64::consts::PI * (self.radius * self.radius)
+    }
+    */
+//}
