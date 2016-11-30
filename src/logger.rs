e815c330e40f9a270b4e6a7752906a16cc610912
Boris Popov <popov@whitekefir.ru>
2016-11-30 13:25:11 +0300

    add syslog wrapper

--- src/logger.rs
+++ src/logger.rs
@@ -7,45 +7,40 @@
 //
 
 extern crate syslog;
-use logger::syslog::{Facility, Severity};
+use self::syslog::{Facility, Severity};
+use self::syslog::unix;
+use std::io;
 
 pub struct Logger {
-
-    name:    &'static str,
-    version: &'static str
+    writer: Box<syslog::Logger>
 }
 
 impl Logger {
-    /*
-    pub fn start(&self) {
-        //self.name = &NAME;
-        //self.version = VERSION.to_string();
-        
-        //self.name = "MountAndExec";
-        //
-        //TODO
-        //
-        //return;
-    }
-    */
-    pub fn new(n: &'static str,
-               v: &'static str) -> Logger {
-
-        match syslog::unix(Facility::LOG_USER) {
-            Err(e)     => println!("impossible to connect to syslog: {:?}", e),
-            Ok(writer) => {
-                let r = writer.send(Severity::LOG_ALERT, "hello world");
-                if r.is_err() {
-                    println!("error sending the log {}", r.err().expect("got error"));
-                }
-            }
+    pub fn new(name: &str) -> Logger {
+        let res = unix(Facility::LOG_USER);
+        if res.is_ok() {
+            let mut w = res.unwrap();
+            w.set_process_name(name.to_string());
+            return Logger{writer: w};
         }
+        panic!("Impossible to connect to syslog!");
+    }
 
+    pub fn info(&self, s: &str) {
+        let res = self.writer.send_3164(Severity::LOG_INFO, s);
+        if res.is_err() {
+            self.to_console(res);
+        }
+    }
 
-        
-        //
-        //TODO make writer
-        //
-        Logger{name: n, version: v}
+    pub fn error(&self, s: &str) {
+        let res = self.writer.send_3164(Severity::LOG_ERR, s);
+        if res.is_err() {
+            self.to_console(res);
+        }
+    }
+
+    fn to_console(&self, r: Result<usize, io::Error>) {
+        println!("Impossible to write to syslog: {:?}", r);
     }
 }
