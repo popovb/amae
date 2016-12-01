cdb94a14bf9ac3087559686e24ea0cb88a1fb364
Boris Popov <popov@whitekefir.ru>
2016-12-01 13:27:13 +0300

    mod env is completed?

--- src/env.rs
+++ src/env.rs
@@ -6,7 +6,12 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
 
+extern crate libc;
+
 use logger;
+use self::libc::getenv;
+
+use std::ffi::CString;
 
 pub struct Env {
     device: String,
@@ -15,34 +20,70 @@ pub struct Env {
 }
 
 impl Env {
-
     pub fn new() -> Env {
         return Env{device: "".to_string(),
                    dir:    "".to_string(),
                    script: "".to_string()}
     }
 
-    pub fn load(&self, l: &logger::Logger) {
+    pub fn load(&mut self, l: &logger::Logger) {
         self.load_device(l);
         self.load_dir(l);
         self.load_script(l);
     }
 
-    fn load_device(&self, l: &logger::Logger) {
-        //
-        //TODO
-        //
+    fn load_device(&mut self, l: &logger::Logger) {
+        self.device = self.load_("AMAE_DEVICE", l);
     }
 
-    fn load_dir(&self, l: &logger::Logger) {
-        //
-        //TODO
-        //
+    fn load_dir(&mut self, l: &logger::Logger) {
+        self.dir = self.load_("AMAE_MOUNT_DIR", l);
     }
 
-    fn load_script(&self, l: &logger::Logger) {
-        //
-        //TODO
-        //
+    fn load_script(&mut self, l: &logger::Logger) {
+        self.script = self.load_("AMAE_SCRIPT", l);
+    }
+
+    fn load_(&self, name: &str, l: &logger::Logger) -> String {
+        let cname = CString::new(name).unwrap();
+        let cname_ptr = cname.as_ptr();
+        unsafe {
+            let out = getenv(cname_ptr);
+            if out.is_null() { self.error_(name, l); }
+            let cstring = CString::from_raw(out);
+            let res = cstring.into_string();
+            match res {
+                Ok(v)  => return v,
+                Err(e) => {
+                    let mut ss = self.ef(name);
+                    ss += " ";
+                    ss += &(format!("{:?}", e));
+                    self.error__(ss, l);
+                    return "".to_string();
+                }
+            }
+        }
+    }
+
+    fn error_(&self, name: &str, l: &logger::Logger) {
+        self.error__(self.ef(name), l);
+    }
+
+    fn ef(&self, name: &str) -> String {
+        let em = "Can't get env: ".to_string() +
+            name + "!";
+        return em;
+    }
+
+    fn error__(&self, s: String, l: &logger::Logger) {
+        l.error(s.as_str());
+        panic!(s);
     }
 }
+/*
+#[test]
+fn hello() {
+    let x = Err("test");
+    assert_eq!("test", x.unwrap());
+}
+ */
