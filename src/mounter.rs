6e31ad030a7ee29b5d3917ce41b3aba41de480a9
Boris Popov <popov@whitekefir.ru>
2016-12-06 11:04:48 +0300

    add Mounter::load_part_info

--- src/mounter.rs
+++ src/mounter.rs
@@ -16,17 +16,21 @@ use blkid;
 
 //////////////////////////////////////////////////////////////////
 pub struct Mounter {
-    label:  Option<String>
+    part_label: String,
+    part_type:  String
 }
 
 impl Mounter {
 
     pub fn new() -> Mounter {
-        Mounter{label: None}
+        Mounter{
+            part_label: "".to_string(),
+            part_type: "".to_string()
+        }
     }
 
     pub fn mount(&mut self, l: &logger::Logger, e: &env::Env) {
-        self.load_label(e.getDevice(), l);
+        self.load_part_info(e.getDevice(), l);
         if !self.verify_label(e.getLabel()) {
             let i = format!("Label does not match {}.", e.getLabel());
             l.info(&i);
@@ -35,29 +39,60 @@ impl Mounter {
         //
         //TODO
         //
-        //self.load_fs_type();
         //self.process();
     }
 
     fn verify_label(&self, templ: &str) -> bool {
-        let lbl :String;
-        match self.label {
-            None        => { return false; },
-            Some(ref l) => { lbl = l.to_string(); }
+        if self.part_label.len() == 0 {
+            return false;
         }
 
         let res = Regex::new(templ);
         match res {
-            Ok(re) => re.is_match(&lbl),
+            Ok(re) => re.is_match(&self.part_label),
             _      => false
         }
     }
 
-    fn load_label(&mut self, dev: &str, l: &logger::Logger) {
-        let prober_r = blkid::Prober::new(dev);
-        //
-        //TODO
-        //
+    fn load_part_info(&mut self, dev: &str, l: &logger::Logger) {
+        let prober = blkid::Prober::new(dev);
+        let res = prober.probe();
+        if let Err(e) = res {
+            l.error(&e);
+            exit(1);
+        }
+        self.loadLabel(&prober, l);
+        self.loadType(&prober, l);
+    }
+
+    fn loadLabel(&mut self, prober: &blkid::Prober,
+                 l: &logger::Logger) {
+        let lr = prober.getLabel();
+        match lr {
+            Ok(o) => {
+                self.part_label = o;
+                let mess = "LABEL=".to_string() + &self.part_label;
+                l.info(&mess);
+            },
+            Err(e) => {
+                l.error(&e);
+            }
+        }
+    }
+
+    fn loadType(&mut self, prober: &blkid::Prober,
+                l: &logger::Logger) {
+        let lr = prober.getType();
+        match lr {
+            Ok(o) => {
+                self.part_type = o;
+                let mess = "TYPE=".to_string() + &self.part_label;
+                l.info(&mess);
+            },
+            Err(e) => {
+                l.error(&e);
+            }
+        }
     }
 }
 //////////////////////////////////////////////////////////////////
@@ -65,49 +100,64 @@ impl Mounter {
 //////////////////////////////////////////////////////////////////
 #[test]
 fn test_verify_label_01() {
-    let i = Mounter{label: Some("BNC-124".to_string())};
+    let i = Mounter{part_label: "BNC-124".to_string(),
+                    part_type: "".to_string()};
     assert_eq!(false, i.verify_label(".*ANC.*"));
 }
 
 #[test]
 fn test_verify_label_02() {
-    let i = Mounter{label: Some("XANC-124".to_string())};
+    let i = Mounter{part_label: "XANC-124".to_string(),
+                    part_type: "".to_string()};
     assert_eq!(true, i.verify_label(".*ANC.*"));
 }
 
 #[test]
 fn test_verify_label_04() {
-    let i = Mounter{label: Some("ANC".to_string())};
+    let i = Mounter{part_label: "ANC".to_string(),
+                    part_type: "".to_string()};
     assert_eq!(true, i.verify_label(".*ANC.*"));
 }
 
 #[test]
 fn test_verify_label_05() {
-    let i = Mounter{label: Some("01234_ ANC".to_string())};
+    let i = Mounter{part_label: "01234_ ANC".to_string(),
+                    part_type: "".to_string()};
     assert_eq!(true, i.verify_label(".*ANC.*"));
 }
 
 #[test]
 fn test_verify_label_06() {
-    let i = Mounter{label: Some("01234_ ANC 782883 ".to_string())};
+    let i = Mounter{part_label: "01234_ ANC 782883 ".to_string(),
+                    part_type: "".to_string()};
     assert_eq!(true, i.verify_label(".*ANC.*"));
 }
 
 #[test]
 fn test_verify_label_07() {
-    let i = Mounter{label: Some(" ANC 782883 ".to_string())};
+    let i = Mounter{part_label: " ANC 782883 ".to_string(),
+                    part_type: "".to_string()};
     assert_eq!(true, i.verify_label(".*ANC.*"));
 }
 
 #[test]
 fn test_verify_label_08() {
-    let i = Mounter{label: Some("ANC---!782883 ".to_string())};
+    let i = Mounter{part_label: "ANC---!782883 ".to_string(),
+                    part_type: "".to_string()};
     assert_eq!(true, i.verify_label(".*ANC.*"));
 }
 
 #[test]
 fn test_verify_label_09() {
-    let i = Mounter{label: Some("01234_ NC 782883 ".to_string())};
+    let i = Mounter{part_label: "01234_ NC 782883 ".to_string(),
+                    part_type: "".to_string()};
+    assert_eq!(false, i.verify_label(".*ANC.*"));
+}
+
+#[test]
+fn test_verify_label_10() {
+    let i = Mounter{part_label: "".to_string(),
+                    part_type: "".to_string()};
     assert_eq!(false, i.verify_label(".*ANC.*"));
 }
 //////////////////////////////////////////////////////////////////
